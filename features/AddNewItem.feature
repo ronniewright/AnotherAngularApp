5ea4fe090ffa07b562742d4f4cd0f5a0030de92e
Ronnie Wright <ronnie.e.wright@gmail.com>
2016-03-29 13:38:31 -0400

    field map in order to use User Friendly names for objects and map them objects in the html

--- features/AddNewItem.feature
+++ features/AddNewItem.feature
@@ -3,10 +3,10 @@ Feature: Add new Items
   As a user
   I would like to be able to add additional items to my to do list
 
-  @watch
+  @addItem
   Scenario: ability to add a new item to the list
     Given I am viewing my to do list
-    When I populate the newItem field with "New Item To Do"
-    And I click the addNewItem button
+    When I populate the New Item field with "New Item To Do"
+    And I click the Add New Item button
     Then "New Item To Do" is added to the list
 
--- features/step_definitions/addItem.js
+++ features/step_definitions/addItem.js
@@ -1,15 +1,19 @@
 module.exports = function(){
+    var fieldMap = {
+        'New Item': 'newItem',
+        'Add New Item': 'addNewItem'
+    };
 
     this.Given(/^I am viewing my to do list$/, function () {
         browser.url('http://localhost:8000/app/index.html')
     });
 
     this.When(/^I populate the (.*) field with "([^"]*)"$/, function (field, arg1) {
-        browser.setValue('input[name="' + field + '"]', arg1);
+        browser.setValue('input[name="' + fieldMap[field] + '"]', arg1);
     });
 
     this.When(/^I click the (.*) button$/, function (field) {
-        browser.click('input[name="' + field + '"]');
+        browser.click('input[name="' + fieldMap[field] + '"]');
     });
 
     this.Then(/^"([^"]*)" is added to the list$/, function (newItemVal) {
